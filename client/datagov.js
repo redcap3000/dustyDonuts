7697355860a041bab8366bb44cba287455e8c468
Ronaldo Barbachano <redcap3000@gmail.com>
2015-03-04 17:36:21 -0800

    reworked to render d3 arch per entry versus all at same time

--- client/datagov.js
+++ client/datagov.js
@@ -5,14 +5,48 @@ var handle = Meteor.subscribe('dataset');
 
 Tracker.autorun(function() {
   if (handle.ready()){
-    Template.d3.destroyed();
-    Template.d3.rendered();
+    var startDate = Session.get("startDate");
+    var endDate = Session.get("endDate");
+    if(typeof startDate == "undefined" && typeof endDate == "undefined"){
+      /// figure out first record sort? safe to sort via timestamp ?
+      var ranges = dataset.find({}).fetch();
+      if(ranges && ranges.length > 0){
+        // these need to be dates to properly filter ....
+        console.log(moment(ranges[0].timestamp));
+        Session.set("startDate",ranges[0].timestamp);
+        Session.set("endDate",ranges.pop().timestamp);
+      }
+
+    }
+    // show key
+//    Template.d3.rendered();
 //    console.log(Posts.find().count());
 //    Session.set("ready",true);
   }else{
 //    Session.set("ready",false)
   }
 });
+
+Template.controls.events({
+  'change .startDate': function (evt,tmpl) {
+    console.log(this);
+    var date = tmpl.find(".startDate");
+    if(typeof date != "undefined" && typeof date.value != "undefined" && date.value != ''){
+      Session.set("startDate",date.value);
+    }
+    console.log(date.value);
+    return true;
+    // ...
+  },
+  'change .endDate': function(evt,tmpl){
+    var date = tmpl.find('.endDate');
+      if(typeof date != "undefined" && typeof date.value != "undefined" && date.value != ''){
+        Session.set("endDate",date.value);
+      }
+      return true;
+    }
+});
+
 
 Template.aggregateData.helpers({
   getData: function () {
@@ -25,7 +59,7 @@ Template.aggregateData.helpers({
 Template.sampleCSV.helpers({
   getRows: function () {
     // ...
-    console.log(dataset.find().fetch());
+  //  console.log(dataset.find().fetch());
     return dataset.find();
   }
 });
@@ -47,10 +81,75 @@ Template.controls.events({
 });
 
 
- Template.d3.destroyed = function () {
-      // ...
-    };
+Template.singlePlot.events({
+  'click .pie': function () {
+    // ...
+    console.log(this.data);
+  }
+});
+Template.singlePlot.destroyed = function(){
+  d3.selectAll(".pie_" +  moment(this.data._id).format('YYYMMDDTHHMMSS') ).remove();
+  d3.selectAll(".arc_" + moment(this.data._id).format('YYYMMDDTHHMMSS') ).remove();
+}
+Template.singlePlot.rendered = function(){
+  //console.log(this);
+  var data = [this.data];
+    var color = d3.scale.ordinal()
+      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
+  color.domain(d3.keys(data[0]).filter(
+    // keys to NOT use
+    function(key){
+      return key !== "city" && key !== "_id" && key !== "op" && key !== "resolution" && key !== "timestamp";
+    })
+  );
+
+  data.forEach(function(d){
+    d.fields = color.domain().map(function(name){
+      if(typeof d[name != "undefined"]){
+        return {name:name, val: parseFloat(d[name])}
+      }
+      // hmmm validation???
+      return {};
+    })
+  });
+  var radius = 74,
+      padding = 10;
+
+  var color = d3.scale.ordinal()
+      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+  var arc = d3.svg.arc()
+      .outerRadius(radius)
+      .innerRadius(radius - 30);
+  var pie = d3.layout.pie()
+      .sort(null)
+      // swap out population value
+      .value(function(d) { console.log(d); return d.val; });
+      // ATTACH A CLICK ACTION
+    var svg = d3.select("body").selectAll(".pie_" + moment(this.data._id).format('YYYMMDDTHHMMSS'))
+        .data(data)
+      .enter().append("svg")
+        .attr("class", "pie")
+        .attr("width", radius * 2)
+        .attr("height", radius * 2)
+      .append("g")
+        .attr("transform", "translate(" + radius + "," + radius + ")");
+
+    svg.selectAll(".arc_" + moment(this.data._id).format('YYYMMDDTHHMMSS') )
+        .data(function(d) { console.log(d); return pie(d.fields); })
+      .enter().append("path")
+        .attr("class", "arc")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.name); });
+
+    svg.append("text")
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(function(d) { console.log(d); return d.timestamp; });
+
+
+};
 Template.d3.rendered = function () {
   console.log('rendered');
 var radius = 74,
@@ -68,17 +167,6 @@ var pie = d3.layout.pie()
     .sort(null)
     // swap out population value
     .value(function(d) { console.log(d); return d.val; });
-
-    /*
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "City"; }));
-
-  data.forEach(function(d) {
-    d.ages = color.domain().map(function(name) {
-
-      return {name: name, population: parseFloat(d[name])};
-    });
-  });
-*/
 
   //console.log(data[0]);
   data = dataset.find().fetch();
@@ -120,25 +208,5 @@ var pie = d3.layout.pie()
       .attr("dy", ".35em")
       .text(function(d) { return d; });
 
-  var svg = d3.select("body").selectAll(".pie")
-      .data(data)
-    .enter().append("svg")
-      .attr("class", "pie")
-      .attr("width", radius * 2)
-      .attr("height", radius * 2)
-    .append("g")
-      .attr("transform", "translate(" + radius + "," + radius + ")");
-
-  svg.selectAll(".arc")
-      .data(function(d) { console.log(d); return pie(d.fields); })
-    .enter().append("path")
-      .attr("class", "arc")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.name); });
-/*
-  svg.append("text")
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { console.log(d); return d.State; });
-*/
+  
 };
