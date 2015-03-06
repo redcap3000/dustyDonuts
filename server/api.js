667626c8be05547f1742f555fbe1e2884a8e077f
Ronaldo Barbachano <redcap3000@gmail.com>
2015-03-06 14:08:30 -0900

    code cleanup and fixing moment + to minus timestamp bug in api call

--- server/api.js
+++ server/api.js
@@ -14,7 +14,6 @@ Meteor.startup(function () {
 
 Meteor.publish("dataset",function(overCity,from,before,fields,op,resolution){
 	// get one day only by default ? 
-
 	return dataset.find({});
 });
 
@@ -23,11 +22,11 @@ Meteor.methods({
 		// ...
 		if(typeof from == "undefined"){
 			//start
-			from = moment().subtract(1,'days').startOf('hour').format();
+			from = moment().subtract(1,'days').startOf('day').format();
 		}
 		if(typeof before == "undefined"){
 			//end today ??
-			before = moment().startOf('hour').format();
+			before = moment().startOf('day').format();
 		}
 		if (typeof fields == "undefined" || fields == null){
 			fields = 'airquality_raw,dust,sound';
@@ -36,7 +35,6 @@ Meteor.methods({
 		if(typeof op == "undefined" || op == null){
 			op = 'mean';
 		}
-
 		if(typeof resolution == 'undefined' || op == null){
 			resolution = '1h'
 		}
@@ -48,65 +46,22 @@ Meteor.methods({
 		// over.city=San Francisco
 		// &from=2015-02-27T08:00:00.000Z&before=2015-02-28T08:00:00.000Z
 		var base_url = 'http://sensor-api.localdata.com/api/v1/aggregations?';
-
-		base_url += 'from=' + from + '&before=' + before + '&fields=' + fields + '&resolution=' + resolution+ '&op=' + op + '&over.city=' + overCity ;
+		// need to replace + with minus  for from before timestamps weird timestamp thing? hmmmm
+		from = from.replace('+','-');
+		before = before.replace('+','-');
+		base_url +=  'op=' + op + '&over.city=' + overCity + '&from=' + from + '&before=' + before +  '&resolution=' + resolution+ '&fields=' + fields  ;
 		console.log(base_url);
-
-//		2015-02-04T00:00:00-0800
-//      &before=2015-02-05T00:00:00-0800
 
 		Meteor.http.get(base_url,false,function(error,response){
 			if(typeof error != "undefined" && typeof response != "undefined" && typeof response.data != "undefined" && typeof response.data.data != "undefined"){
-
 				response.data.data.filter(function(arr){
-					// we have rows and fields... mehhh
-					/*
-					if(typeof arr.rows != "undefined" && typeof arr.fields != "undefined"){
-						// ths is the weird response ...
-						//console.log(arr.fields);
-						// i'm guessing the order of the params are the order in which
-						// you structure &fields=
-						var fSplit = fields.split(',');
-						arr.rows.filter(function(arr2,i){
-								var dbEntry = {};
-								_.pairs(arr2).filter(function(newParam){
-									if(newParam[0] != 'city' && newParam[0] != 'timestamp'){
-										var key = parseInt(newParam[0]);
-										if(key){
-											dbEntry[fSplit[key]] = newParam[1];
-										}else if(newParam[0] == '0'){
-											dbEntry[fSplit[0]] = newParam[1];
-										}
-									}else if(newParam[0] == 'timestamp'){
-										dbEntry._id = newParam[1] + resolution + op;
-
-									}else{
-										dbEntry[newParam[0]] = newParam[1];
-									}
-									// id via timestamp ???
-								});
-								if(dbEntry != {} && typeof dbEntry._id != "undefined"){
-									// if the objet exists and the fields are different...
-									// probaby want to use an update and "$set"
-									dataset.update({_id : dbEntry._id },dbEntry,{upsert : true});
-								}
-							// build a new object....
-						});
-					*/
-						arr._id = arr.timestamp + resolution + op + overCity;
-						arr.timestamp = new Date(arr.timestamp);
-						if(dataset.findOne({_id : arr._id}).length === 0 )
-							dataset.update({_id : arr.timestamp + resolution + op + overCity}, arr,{upsert:true});
-
-					
+					arr._id = arr.timestamp + resolution + op + overCity;
+					arr.timestamp = new Date(arr.timestamp);
+					var lookup = dataset.findOne({ _id : arr._id});
+					return (typeof lookup == "undefined" || !lookup ? dataset.update({_id : arr.timestamp + resolution + op + overCity}, arr,{upsert:true}) : false);
 				});
 			}
 		});
 		return true;
-
-//		http://sensor-api.localdata.com/api/v1/sources/ci4tmxpz8000002w7au38un50/entries.csv?count=1&sort=desc
-//	from: Return responses from the the given time, in ISO 8601 format.
-//	before: Return responses up to the given time, in ISO 8601 format.
-//from=2015-02-04T00:00:00-0800&before=2015-02-05T00:00:00-0800
 	}
 });
