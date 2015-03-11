a8237b32c6b3eeb9fd77e0c5ddc0e5c23f16bbed
Ronaldo Barbachano <redcap3000@gmail.com>
2015-03-11 13:14:18 -0800

    got summary data to animated per city over time

--- client/js/datagov.js
+++ client/js/datagov.js
@@ -78,42 +78,39 @@ Template.aggregateData.rendered = function(){
 
 
 Template.aggregateData.helpers({
-  getSummaryData : function(){
+   getSummaryData : function(){
 
     var byCity = {};
-      var data = dataset.find({},{sort: {timestamp: 1}}).map(function (o) {
+      var data = dataset.find({},{sort: {timestamp: 1}}).fetch().filter(function (o) {
         // ...
      if(typeof byCity[o.city] == "undefined"){
           byCity[o.city] = [];
         }
         byCity[o.city].push(o);
 
-      });;
+      });
 
+      var data = dataset.find().fetch();
 
       var r = [];
       var cities = _.keys(byCity);
 
+ 
+
       if(typeof byCity[cities[0]] != "undefined"){
-        console.log(cities);
-        for (var i = 0; i < cities.length; i++) {
-          console.log(byCity[cities[i]].length);
-          var theCity = {}
-          for(var z = 0; z < byCity[cities[i]].length;z ++){
-            if(z === 0){
-              theCity = byCity[cities[i]][z];
-              theCity.aniValues = [byCity[cities[i]][z]];
-            }else{
-              theCity.aniValues.push(byCity[cities[i]][z]);
+
+        for(var key in byCity){
+          var o = byCity[key][0];  
+          o.aniValues = [];
+          byCity[key].filter(function(obj,i){
+            if(i > 0){
+              o.aniValues.push(obj);
             }
-            if(z === byCity[cities[i]].length - 1 ){
-              console.log("end of cities");
-              console.log(theCity);
-            }
-          }
-          r.push(theCity);
-        };
-          console.log(r);
+
+          });
+          r.push(o);
+        }
+
         return r;
         // now add this data as a marker???
       }else{
@@ -168,99 +165,6 @@ Template.aggregateData.helpers({
 });
 
 
-Template.multiPlot.rendered = function () {
-  // ...
-
-
- if(typeof this.data == "undefined"){
-    return false;
-  }
-  var data = this.data;
-  var color = colorRange(this.data);
-  // build a range for the cities????
-  // dont need a foreach :()
-/*
-
-** Code relating to transitons
-
-.delay(function(d, i) { return i / n * duration; })
-
-*/
-  
-  data.fields = color.domain().map(function(name){
-    if(typeof data[name != "undefined"]){
-      return {name:name, val: parseFloat(data[name]) * (name == 'airquality_raw'? 10 : 1)   }
-    }
-    // hmmm validation???
-    return {};
-  })
-
-  var radius = 55,
-      padding = 0;
-  var GUID = moment(this.data.timestamp).format('YYYMMDDTHHMMSS') + this.data.city.split(' ').join('');
-  var arc = d3.svg.arc()
-      .outerRadius(radius)
-      .innerRadius(radius - 30);
-
-
-  var pie = d3.layout.pie()
-      .sort(null)
-      // swap out population value
-      .value(function(d) { return d.val; });
-
-
-
-
-
-      // ATTACH A CLICK ACTION
-  var svg = d3.select("d3data").append("pie")
-      .data([data])
-    .enter().append("svg")
-      .attr("class", "graph pie_" + GUID)
-      .attr("width", radius * 2)
-      .attr("height", radius * 2)
-    .append("g")
-      .attr("transform", "translate(" + radius + "," + radius + ")");
-
-  svg.selectAll("arc" )
-      .data(function(d) {
-        // returns the size of the arc within the fields
-       return pie(d.fields); 
-     })
-    .enter().append("path")
-      .attr("class", "arc_" + GUID) 
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.name); })
-      .transition().delay(function(d, i) { return i * 500; }).duration(500)
-        .attrTween('d', function(d) {
-             var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
-             return function(t) {
-                 d.endAngle = i(t);
-               return arc(d);
-             }
-        })
-      ;
-  
-  svg.append("text")
-      .attr("dy", ".45em")
-      .style("text-anchor", "middle").style("fill",function(d){
-        if(typeof cityColors[d.city] != "undefined"){
-          return cityColors[d.city];
-        }
-        return 'black';
-      })
-      .text(function(d) { 
-        // color code the city....
-        return (   moment(d.timestamp).format('M-D h a')  ) ; 
-      });
-
-  svg.on('click',function(d){
-    /// toggle action!!
-    // maybe pull up dialog to filter results via time, date, city ...
-      Session.set("selectedTime",moment(d.timestamp).format());
-  }); 
-
-};
 
 Template.singlePlot.destroyed = function(){
   if(typeof this.data == "undefined" && typeof this.city != "undefined"){
@@ -282,7 +186,9 @@ Template.singlePlot.rendered = function(){
   if(typeof this.data == "undefined"){
     return false;
   }
-  console.log(this.data.aniValues);
+
+  var svg = d3.select("#map").select("svg"),g = svg.append("g");
+
   var data = this.data;
   var color = colorRange(this.data);
   // build a range for the cities????
@@ -330,7 +236,7 @@ Template.singlePlot.rendered = function(){
     .append("g")
       .attr("transform", "translate(" + radius + "," + radius + ")");
 
-  svg.selectAll("arc" )
+  var arcG = svg.selectAll("arc" )
       .data(function(d) {
         // returns the size of the arc within the fields
        return pie(d.fields); 
@@ -339,14 +245,7 @@ Template.singlePlot.rendered = function(){
       .attr("class", "arc_" + GUID) 
       .attr("d", arc)
       .style("fill", function(d) { return color(d.data.name); })
-      .transition().delay(function(d, i) { return i * 500; }).duration(500)
-        .attrTween('d', function(d) {
-             var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
-             return function(t) {
-                 d.endAngle = i(t);
-               return arc(d);
-             }
-        }).each(function(d){ this._current = d;})
+      
       ;
   
   svg.append("text")
@@ -361,20 +260,66 @@ Template.singlePlot.rendered = function(){
         // color code the city....
         return (   moment(d.timestamp).format('M-D h a')  ) ; 
       });
+      // set up animation to trigger on click.... hmmm
+    var iCount = 0;
+  
+
+    
 
   svg.on('click',function(d){
-    change();
+  
+
+   
+    var order = 0;
+    if(typeof interval == "undefined"){
+     var interval = Meteor.setInterval(function(){
+      
+      console.log(order);
+      console.log(d.aniValues.length);
+      console.log( d.aniValues[order]);
+      if(order === d.aniValues.length){
+        order = 0;
+      }
+
+        d.aniValues[order].fields = color.domain().map(function(name){
+        if(typeof data[name != "undefined"]){
+          return {name:name, val: parseFloat(data[name]) * (name == 'airquality_raw'? 10 : 1)   }
+        }
+        // hmmm validation???
+        return {};
+      })
+
+           arcG.data(function(z){
+            console.log( pie(d.aniValues[order].fields) );
+            return pie(d.aniValues[order].fields) })
+      .transition().delay(function(d, i) { return i * 500; }).duration(500)
+        .attrTween('d', function(d) {
+             var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
+             return function(t) {
+                 d.endAngle = i(t);
+               return arc(d);
+             }
+        }).each(function(d){ this._current = d;});
+
+        order += 1;
+
+    }, 2000);
+  }else{
+    // clear interval maybe?
+  }
+   
+
+      //return d.aniValues[order];
+  
+    // set up string of animations ???
+    //change();
 
     /// toggle action!!
     // maybe pull up dialog to filter results via time, date, city ...
       Session.set("selectedTime",moment(d.timestamp).format());
+     
 
-    function change() {
-      //clearTimeout(timeout);
-      pie.value(function(d) { console.log(d[value]); return d[value]; }); // change the value function
-      path = path.data(pie); // compute the new angles
-      path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
-    }
+  
 
   }); 
 
