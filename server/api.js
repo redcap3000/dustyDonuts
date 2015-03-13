fa0d06794503dd28b1017ac7efbc128bf559dd4b
Ronaldo Barbachano <redcap3000@gmail.com>
2015-03-13 09:29:37 -0800

    fixed issue with date range conversion in subscripition

--- server/api.js
+++ server/api.js
@@ -1,46 +1,55 @@
 Meteor.startup(function () {
 	// refresh hourly? or more?
 	Meteor.setInterval(function(){
+
 		console.log('doing interval lookup');
-		Meteor.call("govApi","Rio de Janeiro");
-		Meteor.call("govApi","Geneva");
-		Meteor.call("govApi","Boston");
-		Meteor.call("govApi","Bangalore");
-		Meteor.call("govApi","San Francisco");
-		Meteor.call("govApi","Shanghai");
-		Meteor.call("govApi","Singapore");
+		Meteor.call("govApi","Rio de Janeiro",null,null,'5m');
+		Meteor.call("govApi","Geneva",null,null,'5m');
+		Meteor.call("govApi","Boston",null,null,'5m');
+		Meteor.call("govApi","Bangalore",null,null,'5m');
+		Meteor.call("govApi","San Francisco",null,null,'5m');
+		Meteor.call("govApi","Shanghai",null,null,'5m');
+		Meteor.call("govApi","Singapore",null,null,'5m');
 	},60 * 60 * 30);
 });
+Meteor.publish("datasetHiResolution",function(){
+	// only return high resolutions ?
 
+
+
+
+});
 Meteor.publish("dataset",function(overCity,from,before,fields,op,resolution){
 	// get one day only by default ? 
 	return dataset.find({});
 });
 
 Meteor.publish("datasetRange",function(f,b){
-	if(typeof f != "undefined" && typeof b != "undefined"){
-		var from = moment(f);
-		var before = moment(b);
+	if(typeof f != "undefined" && typeof b != "undefined" && f && b){
 
-		if(!moment.isDate(from) || !moment.isDate(before)){
-			console.log("NOT A DATE");
-			console.log(f);
-			console.log(b);
-			return dataset.find();
-		}
-		console.log(from);
-		console.log(before);
-		console.log( dataset.find({timestamp: { $gte: from, $lt: before }} ).fetch() );
-		console.log({timestamp: { $gte: from, $lt: before }});
-		if(from && before){
-			return dataset.find({timestamp: { $gte: from.toDate(), $lt: before.toDate() }} );
-		}else{
-			return dataset.find();
-		}
+		var from = moment(f,"YYYYMMDD").startOf('day');
+		var before = moment(b,"YYYYMMDD").endOf('day');
+
+		//if(!moment.isDate(from) || !moment.isDate(before)){
+		//	console.log("NOT A DATE");
+		//	console.log(f);
+		//	console.log(b);
+		//	from = moment().toDate();
+		//	before = moment().subtract(1,'days').toDate();
+		//	console.log(before);
+		//	console.log("returning default dateset");
+		
+	
+		
 	}else{
-		 console.log('return all');
-		return dataset.find();
+		from = moment().startOf('day');
+		before = moment().subtract(1,'days').endOf('day');
+		console.log('returning single day values');
+
 	}
+		// get only a single day
+		return dataset.find({timestamp: { $gte: from.toDate(), $lt: before.toDate() }} );
+	
 })
 
 Meteor.publish("datasetDigest",function(from,before,op,resolution){
@@ -87,7 +96,7 @@ Meteor.publish("datasetDigest",function(from,before,op,resolution){
 });
 
 Meteor.methods({
-	govApi: function (overCity,fields,op,resultion,from,before) {
+	govApi: function (overCity,fields,op,resolution,from,before) {
 		// ...
 		if(typeof from == "undefined"){
 			//start
@@ -104,7 +113,7 @@ Meteor.methods({
 		if(typeof op == "undefined" || op == null){
 			op = 'mean';
 		}
-		if(typeof resolution == 'undefined' || op == null){
+		if(typeof resolution == 'undefined' || resolution == null){
 			resolution = '1h'
 		}
 		if(typeof overCity == "undefined" || overCity == null){
@@ -124,10 +133,12 @@ Meteor.methods({
 		Meteor.http.get(base_url,false,function(error,response){
 			if(typeof error != "undefined" && typeof response != "undefined" && typeof response.data != "undefined" && typeof response.data.data != "undefined"){
 				response.data.data.filter(function(arr){
-					arr._id = arr.timestamp + resolution + op + overCity;
+					arr.resolution = resolution;
+					arr.city = overCity;
+					arr.op = op;
 					arr.timestamp = new Date(arr.timestamp);
-					var lookup = dataset.findOne({ _id : arr._id});
-					return (typeof lookup == "undefined" || !lookup ? dataset.update({_id : arr.timestamp + resolution + op + overCity}, arr,{upsert:true}) : false);
+					var lookup = dataset.findOne( {resolution: resolution,timestamp : arr.timestamp,city : overCity,op : op} );
+					return (typeof lookup == "undefined" || !lookup ? dataset.update({resolution: resolution,timestamp : arr.timestamp,city : overCity,op : op}, arr,{upsert:true}) : false);
 				});
 			}
 		});
